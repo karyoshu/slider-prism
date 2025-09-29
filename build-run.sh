115cbf9a019a0f177ee07eb009da6d218c494cc8
karyoshu <himanshu-mittal@live.com>
2025-09-29 22:44:12 +0200

    reverted to prismatic engine

--- build-run.sh
+++ build-run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 bash ./build.sh
 rm -rf build/
-rm -rf slider-prism.pdx/ slider-prism.pdx.zip
+rm -rf prismatic-game.pdx/ prismatic-game.pdx.zip
 mkdir build
 cd build
 cmake ..
 make
-mv ../slider-prism.pdx ./
-pdsim ./slider-prism.pdx
+mv ../prismatic-game.pdx ./
+pdsim ./prismatic-game.pdx
--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 rm -rf build/
-rm -rf slider-prism.pdx/ slider-prism.pdx.zip
+rm -rf prismatic-game.pdx/ prismatic-game.pdx.zip
 mkdir build
 cd build
 cmake -DCMAKE_TOOLCHAIN_FILE=$PLAYDATE_SDK_PATH/C_API/buildsupport/arm.cmake ..
 make
-mv ../slider-prism.pdx ./
-zip -r slider-prism.pdx.zip slider-prism.pdx/
+mv ../prismatic-game.pdx ./
+zip -r prismatic-game.pdx.zip prismatic-game.pdx/
